68202742f035bb2cffce5baf62426e8a50ee2d49
Rafael "Toguko" dias <toguko@gmail.com>
2016-11-26 19:46:29 -0200

    Update fresh_install.sh

--- fresh_install.sh
+++ fresh_install.sh
@@ -29,7 +29,7 @@ echo Packages to Remove - SET
 # Packages to Remove
 packages_remove='gscan2pdf pidgin vim.tiny sylpheed'
 
-echo Setting Repositores (Google, SystemBack) - SET
+echo Setting Repositores - Google, SystemBack - SET
 # add repos
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
